0fa28ed25d078d9945124b7f7f195a6c31ca183d
Rashedul Hassan <rashedul@outlook.com>
2023-10-01 11:15:34 +0100

    Init commit hello world

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import TaskList from "./components/TaskList";
 const App = () => {
   return (
     <div className="app">
-      <h1 className="app__title">ToDo App</h1>
-      <Task />
-      <TaskList />
+      Hello world
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import "./index.css";
 import store from "./app/store.js";
 import { Provider } from "react-redux";
 
+// main starter project, connected with redux
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
