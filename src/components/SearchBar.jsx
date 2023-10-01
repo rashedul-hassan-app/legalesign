49385b7a3c8fef4e36b16fb9b39a09c79ecd57d0
Rashedul Hassan <rashedul@outlook.com>
2023-10-01 14:07:12 +0100

    Bug fix: Search feature missing onChange handler

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 const SearchBar = ({ onSearch }) => {
     return (
         <div className="search-container">
-            <input type="text" className="search-input" placeholder="Search..." />
+            <input type="text" className="search-input" placeholder="Search..." onChange={e => onSearch(e.target.value)}/>
         </div>
     );
 }
+
+
 
 export default SearchBar;
